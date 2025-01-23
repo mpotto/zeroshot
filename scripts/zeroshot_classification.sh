c7c4cb4691a4dd58a85d315db9cf59ad66ee791b
Ronak Mehta <rmehta004@gmail.com>
2025-01-22 20:37:16 -0800

    Class conditional prompts.

--- scripts/zeroshot_classification.sh
+++ scripts/zeroshot_classification.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# model='RN50'
-# pretrained='yfcc15m'
+model='RN50'
+pretrained='yfcc15m'
 # model="nllb-clip-base"
 # pretrained="v1"
-model='ViT-B-32'
-pretrained='datacomp_m_s128m_b4k'
+# model='ViT-B-32'
+# pretrained='datacomp_m_s128m_b4k'
 root="/mnt/ssd/ronak/datasets/clip_benchmark"
 dataset="cifar10"
-templates="gpt4"
+templates="cifar10_prompts_full"
 
 clip_benchmark eval --dataset=$dataset \
 --task=zeroshot_classification \
@@ -17,4 +17,4 @@ clip_benchmark eval --dataset=$dataset \
 --output="/home/ronak/zeroshot/output/${model}/${dataset}_${templates}.json" \
 --dataset_root=$root \
 --custom_template_file="/home/ronak/zeroshot/prompts/${templates}.json" \
---batch_size=128
+--batch_size=64
