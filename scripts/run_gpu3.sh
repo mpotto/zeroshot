96ff6e737e3310f67de798389d0c7e5cd77c2b45
Ronak Mehta <rmehta004@gmail.com>
2025-01-25 09:32:23 -0800

    Full list of ImageNet-1k captions.

--- scripts/run_gpu3.sh
+++ scripts/run_gpu3.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-device=3
+# device=3
 
-python scripts/create_ideal_embeddings.py RN50
-python scripts/create_ideal_embeddings.py ViT-B-32
-python scripts/create_ideal_embeddings.py nllb-clip-base
+python scripts/generate_prompts.py --dataset=imagenet1k 
