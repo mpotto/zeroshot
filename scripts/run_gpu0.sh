11754f64e6ee222567da5393d7dc09664c07b799
Ronak Mehta <rmehta004@gmail.com>
2025-03-30 19:43:25 -0700

    linear probe results.

--- scripts/run_gpu0.sh
+++ scripts/run_gpu0.sh
@@ -3,13 +3,19 @@
 device=0
 
 # CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_template.py --dataset imagenet1k --model ViT-B-32 --batch_size 128
-# CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_complexity.py --dataset flowers --model nllb-clip-base --batch_size 128
 # CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_complexity.py --dataset sun397 --model RN50 --batch_size 128
+# CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_retrieval.py --dataset flickr8k --model nllb-clip-base --batch_size 128 --num_seeds 5
+# CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_retrieval.py --dataset mscoco_captions --model nllb-clip-base --batch_size 128 --num_seeds 5
 
-for seed in {0..4}
-do
-    python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform class_weight
-    python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform variance
-    python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform class_weight_variance
-done
+CUDA_VISIBLE_DEVICES=$device python scripts/run_linear_probe.py --dataset dtd --model nllb-clip-base --batch_size 128 --num_seeds 5
+CUDA_VISIBLE_DEVICES=$device python scripts/run_linear_probe.py --dataset flowers --model nllb-clip-base --batch_size 128 --num_seeds 5
+CUDA_VISIBLE_DEVICES=$device python scripts/run_linear_probe.py --dataset fgvc_aircraft --model nllb-clip-base --batch_size 128 --num_seeds 5
+CUDA_VISIBLE_DEVICES=$device python scripts/run_linear_probe.py --dataset sun397 --model nllb-clip-base --batch_size 128 --num_seeds 5
+
+# for seed in {0..4}
+# do
+#     python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform class_weight
+#     python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform variance
+#     python scripts/evaluate_imagenet_captions_.py --model RN50 --device $device --seed $seed --nonuniform class_weight_variance
+# done
 
