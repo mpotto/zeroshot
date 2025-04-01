4c20e975d977bd567ef313bb3610f397079b67ab
Ronak Mehta <rmehta004@gmail.com>
2025-04-01 01:48:30 -0700

    VICReg results.

--- notebooks/simulation_ssl.py
+++ notebooks/simulation_ssl.py
@@ -79,6 +79,60 @@ class MiniCLIP(nn.Module):
         loss = self.loss(logits)
 
         return loss, logits
+    
+class MiniVICReg(nn.Module):
+    def __init__(
+            self,
+            in_features_img, 
+            hidden_size_img, 
+            n_layers_img,
+            in_features_txt, 
+            hidden_size_txt, 
+            n_layers_txt,
+            out_features, 
+            lam,
+        ):
+        super(MiniVICReg, self).__init__()
+        self.image_encoder = MLP(in_features_img, hidden_size_img, n_layers_img, out_features)
+        self.text_encoder  = MLP(in_features_txt, hidden_size_txt, n_layers_txt, out_features)
+        # self.logit_scale = torch.nn.Parameter(0.1 * torch.randn(1)) # learnable parameter
+
+        self.gamma = 1.
+        self.epsilon = 1e-4
+        self.lam = lam
+        self.mu = lam
+        self.nu = 1.
+
+        
+    def forward(self, x, z):
+
+        # extract feature representations of each modality
+        I_f = self.image_encoder(x)
+        T_f = self.text_encoder(z)
+
+        # joint multimodal embedding [n, d_e]
+        # I_e = F.normalize(I_f)
+        # T_e = F.normalize(T_f)
+        I_e = I_f
+        T_e = T_f
+
+        # scaled pairwise cosine similarities [n, n]
+        logits = None
+
+        # eq (6) of https://arxiv.org/pdf/2105.04906
+        loss = self.lam * self._s(I_e, T_e) + self.mu * (self._v(I_e) + self._v(T_e)) + self.nu * (self._c(I_e) + self._c(T_e))
+
+        return loss, logits
+    
+    def _v(self, A):
+        return F.relu(self.gamma - torch.sqrt(A.var(dim=0) + self.epsilon)).mean()
+    
+    def _s(self, A, B):
+        return ((A - B) ** 2).sum() / len(A)
+    
+    def _c(self, A):
+        cov = torch.cov(A)
+        return ((cov - torch.diag(cov)) ** 2).sum() / len(cov)
 
 class MultimodalEmbeddingDataset(Dataset):
     def __init__(self, x, z, y):
@@ -195,6 +249,118 @@ def run_ssl_experiment(p, a, b, a_, b_, setting, seed=123, device="cuda:0"):
         total_acc = 0
         for idx, x, z, y in val_dataloader:
             features = F.normalize(model.image_encoder(x.to(device)))
+            scores = torch.stack([features @ w0, features @ w1])
+            y_pred = torch.argmax(scores, dim=0).cpu()
+            total_acc += ((y == y_pred).sum() / len(y)).item()
+        avg_acc = total_acc / len(val_dataloader)
+
+    return avg_acc
+
+def run_vicreg_experiment(p, a, b, a_, b_, setting, lam, seed=123, device="cuda:0"):
+
+    muX_0, muX_1 = setting["muX_0"], setting["muX_1"]
+    d = setting["d"]
+
+    # data distributions and parametrs
+    muZ_0,  muZ_1, CZZ_0, CZX_0, CZZ_1, CZX_1, CXX_0, CXX_1 = generate_distributions(a, b, a_, b_, setting)
+    CXZ_0 = CZX_0.T
+    CXZ_1 = CZX_1.T
+    mu_0 = torch.cat([muX_0, muZ_0])
+    mu_1 = torch.cat([muX_1, muZ_1])
+    C_0 = torch.cat(
+            [
+                torch.cat([CXX_0, CXZ_0], dim=1),
+                torch.cat([CZX_0, CZZ_0], dim=1)
+            ], dim=0
+        )
+    C_1 = torch.cat(
+        [
+            torch.cat([CXX_1, CXZ_1], dim=1),
+            torch.cat([CZX_1, CZZ_1], dim=1)
+        ], dim=0
+    )
+
+    # samplers
+    mvn_0 = MultivariateNormal(loc=mu_0, covariance_matrix=C_0)
+    mvn_1 = MultivariateNormal(loc=mu_1, covariance_matrix=C_1)
+
+
+    torch.manual_seed(seed)
+
+    # training data
+    n_samples = 10000
+    batch_size = 500
+    n1 = int(n_samples * p)
+    n0 = n_samples - n1
+    v = torch.cat([mvn_0.rsample((n0,)), mvn_1.rsample((n1,))], dim=0)
+    x = v[:, :len(muX_0)]
+    z = v[:, len(muX_0):]
+    y = torch.cat([torch.zeros(n0), torch.ones(n1)]).int()
+    train_dataset = MultimodalEmbeddingDataset(x, z, y)
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, batch_size=batch_size
+    )
+
+    # prompt data
+    n_samples = 500
+    n1 = int(n_samples * p)
+    n0 = n_samples - n1
+    v = torch.cat([mvn_0.rsample((n0,)), mvn_1.rsample((n1,))], dim=0)
+    z0 = v[:n0, len(muX_0):]
+    z1 = v[n0:, len(muX_0):]
+
+    # evaluation data
+    n_samples = 5000
+    batch_size = 500
+    n1 = int(n_samples * p)
+    n0 = n_samples - n1
+    v = torch.cat([mvn_0.rsample((n0,)), mvn_1.rsample((n1,))], dim=0)
+    x = v[:, :len(muX_0)]
+    z = v[:, len(muX_0):]
+    y = torch.cat([torch.zeros(n0), torch.ones(n1)]).int()
+    val_dataset = MultimodalEmbeddingDataset(x, z, y)
+    val_dataloader = DataLoader(
+        val_dataset, shuffle=True, batch_size=batch_size
+    )
+
+    model_cfg = {
+        "in_features_img": d, 
+        "hidden_size_img": 16, 
+        "n_layers_img": 1,
+        "in_features_txt": d, 
+        "hidden_size_txt": 16, 
+        "n_layers_txt": 1,
+        "out_features": 2, 
+        "lam": lam,
+    }
+    model = MiniVICReg(**model_cfg).to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2)
+
+    n_epochs = 30
+    for epoch in range(n_epochs):
+        total_loss = 0
+        for idx, x, z, y in train_dataloader:
+            x = x.to(device)
+            z = z.to(device)
+            loss, logits = model(x, z)
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+            total_loss += loss.item()
+        if epoch % 10 == 0:
+            print(f"\t epoch {epoch + 1:02d}/{n_epochs:02d} loss: {total_loss / len(train_dataloader):0.5f}")
+
+    # generate classifier
+    with torch.no_grad():
+        # w0 = F.normalize(model.text_encoder(z0.to(device))).mean(dim=0)
+        # w1 = F.normalize(model.text_encoder(z1.to(device))).mean(dim=0)
+        w0 = model.text_encoder(z0.to(device)).mean(dim=0)
+        w1 = model.text_encoder(z1.to(device)).mean(dim=0)
+        total_acc = 0
+        for idx, x, z, y in val_dataloader:
+            # features = F.normalize(model.image_encoder(x.to(device)))
+            features = model.image_encoder(x.to(device))
             scores = torch.stack([features @ w0, features @ w1])
             y_pred = torch.argmax(scores, dim=0).cpu()
             total_acc += ((y == y_pred).sum() / len(y)).item()
---
+++ scripts/run_simulation_vicreg.py
@@ -0,0 +1,42 @@
+import math
+import torch
+from tqdm import tqdm
+
+import sys
+sys.path.extend([".", ".."])
+
+from notebooks.simulation_ssl import run_vicreg_experiment
+
+p = 0.5
+a = 5
+b = 6
+seeds = torch.arange(10)
+
+rho = 0.5
+diff = 0.7
+d = 2
+muX_0 = torch.ones(d) * diff
+muX_1 = -torch.ones(d) * diff
+
+setting = {
+    "rho": rho,
+    "diff": diff,
+    "d": d,
+    "muX_0": torch.ones(d) * diff,
+    "muX_1":  -torch.ones(d) * diff
+}
+
+lam = 25
+
+props = torch.load(f"notebooks/output/props_{d}.pt")
+
+for seed in seeds:
+    print(f"\t running seed {seed}...")
+    acc3s = []
+    for prop in tqdm(props):
+        # print(f"control parameter = {prop:0.3f}")
+        a_ = prop * a
+        b_ = math.sqrt(prop) * b
+        acc3 = run_vicreg_experiment(p, a, b, a_, b_, setting, lam, seed=seed)
+        acc3s.append(acc3)
+    torch.save(torch.tensor(acc3s), f"notebooks/output/vicreg_accuracies_{d}_seed_{seed}.pt")
