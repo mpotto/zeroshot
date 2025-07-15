14499154fae88899aa25a784428c670a460a6582
Ronak Mehta <rmehta004@gmail.com>
2025-07-15 11:45:21 -0700

    Clean up repo files.

--- miniclip/run_gpu3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# device=3
-
-CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_complexity.py --dataset fgvc_aircraft --model ViT-B-32 --batch_size 128
-CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_complexity.py --dataset fgvc_aircraft --model nllb-clip-base --batch_size 128
-CUDA_VISIBLE_DEVICES=$device python scripts/run_zeroshot_complexity.py --dataset fgvc_aircraft --model RN50 --batch_size 128
